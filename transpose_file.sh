2461159aebe23df1715795df62c389c7ce2a5221
Inderdip Shere <92054336+InderdipShere@users.noreply.github.com>
2021-10-07 22:47:24 +0530

    Add files via upload
    This batch script is useful to transpose the contains of the file. 
    For example, if the files contains the rows of data in an single column, this program will convert it into single row of multiple column and vice versa.
    This is useful for giving variable directly to next script.

---
+++ transpose_file.sh
@@ -0,0 +1,19 @@
+cp $1 awk_temp.dat
+awk '
+{ 
+    for (i=1; i<=NF; i++)  {
+        a[NR,i] = $i
+    }
+}
+NF>p { p = NF }
+END {    
+    for(j=1; j<=p; j++) {
+        str=a[1,j]
+        for(i=2; i<=NR; i++){
+            str=str" "a[i,j];
+        }
+        print str
+    }
+}' awk_temp.dat > $1
+
+rm awk_temp.dat
